031292e142a09164bd98a75e8e5418836a0021bc
veiculo <castelo.rubinho@gmail.com>
2025-06-23 17:32:00 -0300

    Arquivo adicionado

---
+++ sw.js
@@ -0,0 +1,20 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("veiculos-cache").then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png",
+        "./app.js"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
